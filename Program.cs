9d24e5245ae0721c917a4de9ef0ffa95c54fd479
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-05 14:53:29 +0530

    child domain

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ var path = "GC://dom051902.lab";
 
 try {
     using (var root = new DirectoryEntry(path,"DOM051902\\Administrator","Control123")) {
-        foreach(DirectoryEntry child in root.Children) {
-            Console.WriteLine(child.Name);
+        foreach(var property in root.Properties.PropertyNames) {
+            Console.WriteLine(property.ToString());
         }
     }
 
