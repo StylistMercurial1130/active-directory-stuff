e473030cd81a9cf69ea025e9aa20a90bbc6a2b95
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-05 14:59:18 +0530

    domain api

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
 ﻿using System.DirectoryServices;
-using Microsoft.Win32;
+using System.DirectoryServices.ActiveDirectory;
 
-var path = "GC://dom051902.lab";
+var domain = Domain.GetDomain(
+    new DirectoryContext(
+        DirectoryContextType.Domain,
+        "dom051902.lab",
+        "DOM051902\\Administrator","Control123")
+);
 
-try {
-    using (var root = new DirectoryEntry(path,"DOM051902\\Administrator","Control123")) {
-        foreach(var property in root.Properties.PropertyNames) {
-            Console.WriteLine(property.ToString());
-        }
-    }
-
-} catch (DirectoryServicesCOMException) {
-
-}
