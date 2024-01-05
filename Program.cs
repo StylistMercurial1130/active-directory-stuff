c378509814694737a0ff90406ccea214e950b72c
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-05 12:20:01 +0530

    connection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.DirectoryServices;
+
+DirectoryEntry connection() {
+    return new DirectoryEntry("LDAP://dom051902.lab","DOM051902\\Administrator","Control123",AuthenticationTypes.Secure);
+}
