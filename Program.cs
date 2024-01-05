e2408ed4395828e1afb938284cfafac643b87cc6
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-05 14:49:57 +0530

    child domain

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ var path = "GC://dom051902.lab";
 
 try {
     using (var root = new DirectoryEntry(path,"DOM051902\\Administrator","Control123")) {
-        Console.WriteLine(root.Name);
+        foreach(DirectoryEntry child in root.Children) {
+            Console.WriteLine(child);
+        }
     }
 
 } catch (DirectoryServicesCOMException) {
