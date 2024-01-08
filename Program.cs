95b6526e8ba27705164c9798f549b97f9fe65055
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-08 10:17:14 +0530

    ldap objects

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.DirectoryServices.ActiveDirectory;
 
 class Program {
     public static void Main(String []args) {
-        var path = "GC://dom051902.lab";
+        var path = "LDAP://dom051902.lab";
         var username = "DOM051902\\Administrator"; 
         var password = "Control123";
         DirectoryEntry directoryEntry = 
