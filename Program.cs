9d077920128107be9dd57d137d1ea635b5493503
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-08 17:58:42 +0530

    directory entry of root domain

--- Program.cs
+++ Program.cs
@@ -1,18 +1,17 @@
 ﻿using System.DirectoryServices;
-using System.Security.Principal;
+using System.DirectoryServices.ActiveDirectory;
 
 class Program {
     public static void Main(String []args) {
-        var path = "LDAP://dom051902.lab/CN=Computers";
+        // var path = "LDAP://dom051902.lab/CN=Computers";
         var username = "DOM051902\\Administrator"; 
         var password = "Control123";
-        DirectoryEntry directoryEntry = 
-            new DirectoryEntry(
-                path,username,password,AuthenticationTypes.Secure
-            );
-		foreach(DirectoryEntry child in directoryEntry.Children) {
-			string computerName = child.Name;
-			if(computerName != "") Console.WriteLine(computerName);
-		}
+     
+		DirectoryContext context = new DirectoryContext(DirectoryContextType.Forest,username,password);	
+		Forest forest = Domain.GetDomain(context).Forest;
+
+		DirectoryEntry de = forest.RootDomain.GetDirectoryEntry();
+
+		Console.WriteLine(de.Name);
     }
 }
