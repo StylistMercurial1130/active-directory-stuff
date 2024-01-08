0d4740b76f806c77d8669034d46325cc3be02500
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-08 14:48:29 +0530

    comkputer search

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Security.Principal;
 
 class Program {
     public static void Main(String []args) {
-        var path = "LDAP://dom051902.lab";
+        var path = "LDAP://dom051902.lab/CN=Computers";
         var username = "DOM051902\\Administrator"; 
         var password = "Control123";
         DirectoryEntry directoryEntry = 
