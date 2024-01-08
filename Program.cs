a5f3f171b5dab18cae79e3242774cb013c81eaf4
Karthik Venkatesh <kvenkatesh@opentext.com>
2024-01-08 18:01:49 +0530

    directory entry for root domain

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@ using System.DirectoryServices.ActiveDirectory;
 
 class Program {
     public static void Main(String []args) {
-        // var path = "LDAP://dom051902.lab/CN=Computers";
+        var path = "dom051902.lab";
         var username = "DOM051902\\Administrator"; 
         var password = "Control123";
      
-		DirectoryContext context = new DirectoryContext(DirectoryContextType.Forest,username,password);	
+		DirectoryContext context = new DirectoryContext(DirectoryContextType.Forest,path,username,password);	
 		Forest forest = Domain.GetDomain(context).Forest;
 
 		DirectoryEntry de = forest.RootDomain.GetDirectoryEntry();
