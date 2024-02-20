88b4064d271bd7cf59b76ab5794fcac84c23e343
Karthik.Venkatesh.76010858 <Karthik.Venkatesh@microfocus.com>
2024-02-20 11:38:53 +0530

    serverless binding

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Object;
 
 public class Program {
     public static void Main(string []args) {
-		const string ldapPath = "LDAP://dom051903/dom051901.lab/DC=dom051903,DC=dom051901,DC=lab";
+		const string ldapPath = "LDAP://dom051903.dom051901.lab/DC=dom051903,DC=dom051901,DC=lab";
 		const string account = "Administrator";
 		const string password = "Control123";
     	DirectoryEntry de = new DirectoryEntry(ldapPath,account,password,AuthenticationTypes.Secure); 
